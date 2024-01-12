ff23610d41d44d06fda0712d8354ba590de60a11
riumr <atrium_grate.0j@icloud.com>
2024-01-12 23:56:39 +0900

    setting : env 값 설정 및 supabase DB 가져오기

--- supabase_crud/src/App.js
+++ supabase_crud/src/App.js
@@ -1,7 +1,25 @@
+const { createClient } = require('@supabase/supabase-js')
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
 function App() {
+  const main = async () => {
+    let { data, error } = await supabase
+      .from('newTable')
+      .select("*")
+
+    if (error) {
+      console.error(error)
+    }
+
+    console.log(data)
+  }
+
+  main()
   return (
     <div className="App">
-  
     </div>
   );
 }
