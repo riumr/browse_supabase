e8e87c0ed31ed25aa4ae5b8d0ffc0f4a7968c0d6
riumr <atrium_grate.0j@icloud.com>
2024-01-15 23:08:36 +0900

    setting : supabase 초기화 파일 생성

---
+++ supabase_crud/src/components/supabase.js
@@ -0,0 +1,5 @@
+const { createClient } = require('@supabase/supabase-js');
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
