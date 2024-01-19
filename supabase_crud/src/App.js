6554b7a8e5377989b8c784f13ce183332e3b831f
riumr <atrium_grate.0j@icloud.com>
2024-01-19 23:56:47 +0900

    feat : Post 컴포넌트 내에 input 필드 추가
    - 기존 Form 컴포넌트 삭제

--- supabase_crud/src/App.js
+++ supabase_crud/src/App.js
@@ -1,13 +1,11 @@
 import Read from "./components/Read";
 import Post from "./components/Post";
 import Edit from "./components/Edit";
-import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <h1>Supabase Data</h1>
-      <Form />
       <Read />
       <Post />
       <Edit />
--- supabase_crud/src/components/Post.js
+++ supabase_crud/src/components/Post.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { supabase } from "./supabase"
 
 export default function Post() {
+    const [rowId, setRowId] = useState(0);
+    const [rowText, setRowText] = useState('');
+    const post = async () => {
+        await supabase
+            .from('newTable')
+            .insert({ id: rowId, oneColumn: rowText })
+    }
     return (
         <div>
+            <label>
+                rowId:
+                <input
+                    value={rowId}
+                    onChange={e => setRowId(e.target.value)}
+                    type="number"
+                />
+            </label>
+            <label>
+                rowText:
+                <input
+                    value={rowText}
+                    onChange={e => setRowText(e.target.value)}
+                />
+            </label>
             <button onClick={post}>post</button>
         </div>
     )
 }
 
-const post = async () => {
-    await supabase
-        .from('newTable')
-        .insert({ id: "5", oneColumn: 'added' })
-}
