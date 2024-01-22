40fc964f5cc7d521ad3918232c6fbed318cb8199
riumr <atrium_grate.0j@icloud.com>
2024-01-22 23:59:05 +0900

    feat : 열 click시 input 요소로 변해 수정할 수 있게 하는 코드 추가

--- supabase_crud/src/components/Read.js
+++ supabase_crud/src/components/Read.js
@@ -3,6 +3,7 @@ import { supabase } from './supabase';
 
 export default function Read() {
     const [data, setData] = useState([]);
+    const [isEditing, setEditing] = useState(false);
     useEffect(() => {
         fetchData()
     }, []);
@@ -21,18 +22,47 @@ export default function Read() {
         }
     }
     fetchData()
+
+    // 열 삭제
     const deleteRow = async (rowId) => {
         await supabase
             .from('newTable')
             .delete()
             .eq('id', rowId)
     }
+
+    // 열 수정
+    const editRow = async (rowId) => {
+        await supabase
+            .from('newTable')
+            .update({ 'oneColumn': 'column changed' })
+            .eq('id', rowId)
+    }
+
+    const handleEditClick = () => {
+        setEditing(true);
+    };
+
+    const handleBlur = () => {
+        setEditing(false);
+    };
+
     return (
         <ul>
             {data.map((item) => (
                 <>
-                    <li key={item.id}>{item.oneColumn}</li>
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            value={item.oneColumn}
+                            onChange={editRow}
+                            onBlur={handleBlur}
+                        />
+                    ) : (
+                        <li key={item.id} onClick={handleEditClick}>{item.oneColumn}</li>
+                    )}
                     <button onClick={() => deleteRow(item.id)}>Delete</button>
+                    <button onClick={() => editRow(item.id)}>Edit</button>
                 </>
             ))}
         </ul>
