1f1746be1fcdef00eb8e7013c8b6e51c6c046bfb
riumr <atrium_grate.0j@icloud.com>
2024-01-27 23:50:09 +0900

    style : App 컴포넌트 및 내부 컴포넌트 레이아웃 설정
    - 너비, 높이, 테두리

--- supabase_crud/src/App.js
+++ supabase_crud/src/App.js
@@ -4,8 +4,7 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Supabase Data</h1>
+      <div className="wrap">
         <Read />
       </div>
     </div>
