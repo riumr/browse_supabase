59f5b3298fe51d69227c516b26789b15c3557ae8
riumr <atrium_grate.0j@icloud.com>
2024-01-27 23:52:00 +0900

    style : PostForm 컴포넌트 input, button 요소 너비, 높이 등 추가

--- supabase_crud/src/components/PostForm.js
+++ supabase_crud/src/components/PostForm.js
@@ -10,24 +10,45 @@ export default function PostForm({ submitFunction }) {
         setRowText("")
     }
 
+    // 스타일
+    const inputIdStyle = {
+        width: '100%',
+        padding: '0 0 0 0',
+        margin: '20px 0 0 0',
+        border: 'none',
+    }
+
+    const inputTextStyle = {
+        width: '100%',
+        height: '80px',
+        padding: '0 0 0 0',
+        margin: '0 0 0 0',
+        border: 'none',
+        verticalAlign: 'top',
+    }
+
+    const postButtonStyle = {
+        width: '100%',
+    }
+
     return (
         <div>
-            <label>
-                rowId:
+            <div>
                 <input
+                    style={inputIdStyle}
                     value={rowId}
                     onChange={e => setRowId(e.target.value)}
                     type="number"
                 />
-            </label>
-            <label>
-                rowText:
+            </div>
+            <div>
                 <input
+                    style={inputTextStyle}
                     value={rowText}
                     onChange={e => setRowText(e.target.value)}
                 />
-            </label>
-            <button onClick={handleOnclickEvent}>post</button>
+            </div>
+            <button style={postButtonStyle} onClick={handleOnclickEvent}>post</button>
         </div>
     )
 }
